2a36a04cc11dcd99215c29e7b5ed6055c9e991e8
Dzmitry Karneyenka <Dzmitry_Karneyenka@epam.com>
2018-09-12 16:41:07 +0300

    create the framework for unit tests

--- test/unit/calculator.js
+++ test/unit/calculator.js
@@ -1,9 +1,22 @@
 'use strict';
 
-const calculator = new (require('../../index'))();
+const Calculator = require('../../index');
+const { expect } = require('chai');
 
-describe('The first test', () => {
-    it('the first it', () => {
-        console.log(calculator.add(1, 2, 3, 4, 5));
+describe('tests [add] method of Calculator.prototype ', () => {
+    let calculator;
+
+    beforeEach(() => {
+        calculator = new Calculator();
+    });
+
+    afterEach(() => {
+        calculator = null;
+    });
+
+    it('should return [result of summ the first five numbers]', () => {
+        const actualResult = calculator.add(1, 2, 3, 4, 5);
+        expect(actualResult).to.be.a('Number');
+        expect(actualResult).to.be.equal(15);
     });
 });
